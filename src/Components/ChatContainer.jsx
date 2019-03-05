0c26b6655ef7b4395c8127f92873ff27ee3e502a
Duncan Ritchie <codenation@Codes-MBP.lan>
2019-03-05 13:13:03 +0000

    Added another message.

--- src/Components/ChatContainer.jsx
+++ src/Components/ChatContainer.jsx
@@ -55,6 +55,13 @@ class ChatContainer extends React.Component {
             key:104,
         },
         {
+            avatarUrl:"https://farm2.staticflickr.com/1142/1139134023_c497d6b907_z.jpg",
+            authorName:"Dog",
+            date:"2019 Mar 04 11:36:27",
+            message:"Woof woof. Woof woof. Woof woof woof woof, woof woof woof woof woof woof: woof woof woof woof woof woof woof. Woof woof woof woof woof woof woof woof woof woof (woof woof woof woof woof woof woof woof woof woof woof woof woof woof) woof woof woof woof woof; woof woof woof woof woof woof woof woof woof woof woof woof woof woof woof woof! Woof woof woof woof woof woof woof woof woof woof woof woof woof woof woof woof woof.",
+            key:104,
+        },
+        {
             avatarUrl:"https://farm5.staticflickr.com/4447/37761450081_abd8bce5f8_k.jpg",
             authorName:"Cat",
             date:"2019 Mar 04 11:38:20",
@@ -107,6 +114,7 @@ class ChatContainer extends React.Component {
             <ChatRow avatarUrl={msgs[9].avatarUrl} authorName={msgs[9].authorName} date={msgs[9].date} message={msgs[9].message}/>
             <ChatRow avatarUrl={msgs[10].avatarUrl} authorName={msgs[10].authorName} date={msgs[10].date} message={msgs[10].message}/>
             <ChatRow avatarUrl={msgs[11].avatarUrl} authorName={msgs[11].authorName} date={msgs[11].date} message={msgs[11].message}/>
+            <ChatRow avatarUrl={msgs[12].avatarUrl} authorName={msgs[12].authorName} date={msgs[12].date} message={msgs[12].message}/>
         </div>
         );
     }
