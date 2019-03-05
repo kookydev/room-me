1f0b3eb5f0fd7b6afd768b20d3a6404e0fd76637
Duncan Ritchie <codenation@Codes-MBP.lan>
2019-03-05 13:51:53 +0000

    Simplified the chatrows to a .map() function.

--- src/Components/ChatContainer.jsx
+++ src/Components/ChatContainer.jsx
@@ -99,22 +99,11 @@ class ChatContainer extends React.Component {
       ]
     };
     render() {
-    let msgs = this.state.messages;
+        let msgs = this.state.messages;
+        let chatrows = msgs.map((i)=><ChatRow avatarUrl={i.avatarUrl} authorName={i.authorName} date={i.date} message={i.message}/>);
         return (
         <div className="chat-container">
-            <ChatRow avatarUrl={msgs[0].avatarUrl} authorName={msgs[0].authorName} date={msgs[0].date} message={msgs[0].message}/>
-            <ChatRow avatarUrl={msgs[1].avatarUrl} authorName={msgs[1].authorName} date={msgs[1].date} message={msgs[1].message}/>
-            <ChatRow avatarUrl={msgs[2].avatarUrl} authorName={msgs[2].authorName} date={msgs[2].date} message={msgs[2].message}/>
-            <ChatRow avatarUrl={msgs[3].avatarUrl} authorName={msgs[3].authorName} date={msgs[3].date} message={msgs[3].message}/>
-            <ChatRow avatarUrl={msgs[4].avatarUrl} authorName={msgs[4].authorName} date={msgs[4].date} message={msgs[4].message}/>
-            <ChatRow avatarUrl={msgs[5].avatarUrl} authorName={msgs[5].authorName} date={msgs[5].date} message={msgs[5].message}/>
-            <ChatRow avatarUrl={msgs[6].avatarUrl} authorName={msgs[6].authorName} date={msgs[6].date} message={msgs[6].message}/>
-            <ChatRow avatarUrl={msgs[7].avatarUrl} authorName={msgs[7].authorName} date={msgs[7].date} message={msgs[7].message}/>
-            <ChatRow avatarUrl={msgs[8].avatarUrl} authorName={msgs[8].authorName} date={msgs[8].date} message={msgs[8].message}/>
-            <ChatRow avatarUrl={msgs[9].avatarUrl} authorName={msgs[9].authorName} date={msgs[9].date} message={msgs[9].message}/>
-            <ChatRow avatarUrl={msgs[10].avatarUrl} authorName={msgs[10].authorName} date={msgs[10].date} message={msgs[10].message}/>
-            <ChatRow avatarUrl={msgs[11].avatarUrl} authorName={msgs[11].authorName} date={msgs[11].date} message={msgs[11].message}/>
-            <ChatRow avatarUrl={msgs[12].avatarUrl} authorName={msgs[12].authorName} date={msgs[12].date} message={msgs[12].message}/>
+            {chatrows}
         </div>
         );
     }
