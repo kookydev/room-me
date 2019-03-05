0f0ee18ecc24d590f43988acc74afbc1c84262f7
Kyle <kwjtdev@gmail.com>
2019-03-05 22:07:03 +0000

    Final Commit

--- src/Components/HeaderBar.jsx
+++ src/Components/HeaderBar.jsx
@@ -7,29 +7,35 @@ import LogoGrid from "./LogoGrid";
 export class HeaderBar extends Component {
   state = {
     isActive: false
-  }
+  };
 
+  click = () => {
+    this.state.isActive
+      ? this.setState({ isActive: false })
+      : this.setState({ isActive: true });
+  };
 
- click = () => {
-  this.state.isActive ? this.setState({isActive:false}) : this.setState({isActive:true})
-}
-
-
- 
   render() {
-  //   let className = 'top-bar';
-  // if (this.state.isActive) {
-  //   className += ' notification-expand';
-  // }
+    //   let className = 'top-bar';
+    // if (this.state.isActive) {
+    //   className += ' notification-expand';
+    // }
     return (
-      <div className={this.state.isActive ? "top-bar-container notification-expand" : "top-bar-container"}>
-      <div className="top-bar">
-        <LogoGrid />
-        <h1 class="room-name">Room Name</h1>
-        <span className="icons-right">
-          <SearchLogo />
-          <HeadPhonesLogo />
-        </span>
+      <div
+        className={
+          this.state.isActive
+            ? "top-bar-container notification-expand"
+            : "top-bar-container"
+        }
+      >
+        <div className="top-bar">
+          <LogoGrid />
+          <h1 class="room-name">Room Name</h1>
+          <span className="icons-right">
+            <SearchLogo />
+            <HeadPhonesLogo />
+          </span>
+        </div>{" "}
       </div>
     );
   }
