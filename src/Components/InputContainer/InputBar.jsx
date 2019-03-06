07f3b24c404c57a82e68eb9408f3f41b4cca267e
Code Nation 13 <codenation@CN-13.local>
2019-03-06 16:08:16 +0000

    Fixed naming issues

--- src/Components/InputContainer/InputBar.jsx
+++ src/Components/InputContainer/InputBar.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import Input from "./input";
 import PlusButton from "./PlusButton";
 import SubmitButton from "./SubmitButton";
