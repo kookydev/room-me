d291c89bc22c030614a8ca712200d04e80849512
Kyle <kwjtdev@gmail.com>
2019-03-06 20:38:11 +0000

    Progress made migrating components into subfolders

--- src/Components/Notifications.jsx
+++ src/Components/Notifications.jsx
@@ -1,14 +1,16 @@
-import React, { Component } from 'react'
-import YellowBubble from './NotificationBubble'
-import Avatar from './Avatar.jsx'
-import './notification.css'
+import React, { Component } from "react";
+import YellowBubble from "./NotificationBubble";
+import Avatar from "./Avatar.jsx";
+import "./notification.css";
 
-let Notifications = (props) => {
-    return (
-      <div class="notification-bar">
+let Notifications = props => {
+  return (
+    <div class="notification-bar">
       <YellowBubble />
       <h1 class="room-name">Room Name</h1>
       <Avatar />
-      </div>
-    )
-}
+    </div>
+  );
+};
+
+export default Notifications;
