df6e410af3871ff133d0876f7565ed256622f103
CN 8 <administrator@CN-8.lan>
2019-03-06 15:15:44 +0000

    additions

---
+++ src/Components/InputContainer/PaperClipOptions.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class PaperClipOptions extends Component {
+  
+  render() {
+    return (
+      <div>
+        <ul>
+            <li>Upload</li>
+            <li>Emojis</li>
+            <li>Gifs</li>
+            <li>Gallery</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+
+export default PaperClipOptions
